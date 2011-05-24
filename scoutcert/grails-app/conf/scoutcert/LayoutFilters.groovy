042e96b542234776f1bb1471d4587b428967d2b5
Eric <smartytime@gmail.com>
2011-05-24 02:03:25 -0700

    Readded the filters to allow autodetection of iPhones

--- scoutcert/grails-app/conf/scoutcert/LayoutFilters.groovy
+++ scoutcert/grails-app/conf/scoutcert/LayoutFilters.groovy
@@ -10,22 +10,22 @@ class LayoutFilters {
         all(controller: '*', action: '*') {
             before = {
 
-                if (params.isMobile) {
+//                if (params.isMobile) {
+//                    session.isMobile = true
+//                    request.layoutName = "iwebkit"
+//                } else if (!session.isMobile) {
+//                    session.isMobile = false
+//                    request.layoutName = "main"
+//                } else {
+//                    request.layoutName = "iwebkit"
+//                }
+                if (userAgentIdentService.getPlatform() == "iPhone" || params.isMobile || session.isMobile) {
                     session.isMobile = true
                     request.layoutName = "iwebkit"
-                } else if(!session.isMobile) {
+                } else {
                     session.isMobile = false
                     request.layoutName = "main"
-                } else {
-                    request.layoutName = "iwebkit"
                 }
-//                if(userAgentIdentService.getPlatform() == "iPhone" || params.isMobile || session.isMobile) {
-                //                    session.isMobile = true
-                //                    request.layoutName = "iwebkit"
-                //                } else {
-                //                    session.isMobile = false
-                //                    request.layoutName = "main"
-                //                }
 
             }
             after = {
