bff7262dd15ba22e1029965e98732d219fc511a2
ScoutingHub <scoutinghub@infusionsoft.com>
2011-06-05 15:40:21 -0700

    Official release 0.7

--- deploy-and-increment.sh
+++ deploy-and-increment.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-grails maven-deploy --repository=scoutinghub && grails increment-version && git add -u && git commit
+grails clean && grails maven-deploy --repository=scoutinghub && grails increment-version && git add -u && git commit && git push origin master
