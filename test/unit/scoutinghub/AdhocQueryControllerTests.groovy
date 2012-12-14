b705e3f01643c20cf57bc910c6a1adfd40cdc6fd
Lennard Fuller <sparhk@gmail.com>
2012-12-14 09:28:37 -0700

    Quick and dirty support for admin adhoc queries to meet council needs

---
+++ test/unit/scoutinghub/AdhocQueryControllerTests.groovy
@@ -0,0 +1,17 @@
+package scoutinghub
+
+import grails.test.*
+
+class AdhocQueryControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
